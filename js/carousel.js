3839ce38e98130ba333ae21c4a3f99655d413ae5
Tomás Murphy <tomas.murphy.tm@gmail.com>
2022-03-31 21:02:51 -0300

    Ahora con SASS

--- js/carousel.js
+++ js/carousel.js
@@ -1,12 +1,6 @@
+let image = document.querySelector('.image');
+let select = document.getElementById('effect');
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-  });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.carousel').carousel();
-  });
-      
+select.addEventListener('change', function (e) {
+  image.className = `image ${this.value}`;
+});
