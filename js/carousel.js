9ab2a6e5796cae4dbc3822e7325c0b1db5939973
Tomás Murphy <tomas.murphy.tm@gmail.com>
2022-04-03 18:05:04 -0300

    mejorado cv

--- js/carousel.js
+++ js/carousel.js
@@ -4,3 +4,10 @@ let select = document.getElementById('effect');
 select.addEventListener('change', function (e) {
   image.className = `image ${this.value}`;
 });
+var exampleEl = document.getElementById('example')
+var tooltip = new bootstrap.Tooltip(exampleEl, {
+  boundary: document.body // or document.querySelector('#boundary')
+})
+$('#myCollapsible').on('hidden.bs.collapse', function () {
+  // do something…
+})
